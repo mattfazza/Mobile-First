6a2e873bd82214510a64ff99ac6a6bf4463ebc32
mattfazza <mathewsfazza@gmail.com>
2018-12-30 21:02:53 -0600

    Fixed navbar, problems with animations, and dynamic background image

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -6,16 +6,11 @@ class LandingPage extends Component {
     render(){
         return(
             <div className="landing-container">
-                <div className="row">
-                    <div class="col-12">
-                        <img src={require("../img/bean_large.jpg")} class="img-fluid" alt="Responsive image" />
+ 
+                        {/* <h1 className = "main-Title">Matt Fazza</h1>
+                        <h3 className = "dotted-Line"/>
+                        <h2 className = "sub-Title">Chicago-based Full Stack Engineer</h2> */}
 
-                        <h1 className = "main-Title">Matt Fazza</h1>
-                        <hr className = "dotted-Line"/>
-                        <h2 className = "sub-Title">Chicago-based Full Stack Engineer</h2>
-
-                    </div>
-                </div>
             </div>
         )
     }
