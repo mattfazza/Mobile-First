7da1511b84012fe7e014c0696cf31da08175ef28
mattfazza <mathewsfazza@gmail.com>
2019-01-01 20:07:34 -0600

    Started working on the aboutme section

--- src/components/about.js
+++ src/components/about.js
@@ -1,20 +1,41 @@
 import React, { Component } from "react";
+import { Cell, Grid } from "react-mdl";
 
 class About extends Component {
 
     render(){
         return(
             <div className = "about-outer">
-                  <div className="landing-container">
-                        <div className="row">
-                            <div class="col-6">
-                                LEFT SIDE
-                            </div>
-                            <div class="col-6">
-                                RIGHT SIDE
+                <Grid className="landing-grid">
+                    <Cell col={12}>
+                        <img 
+                            src={require("../img/linkedin.png")}
+                            alt="avatar"
+                            className="avatar-img"
+                        />
+
+                        <p></p>
+                        <div className="banner-text">
+                            <h1>Full Stack Engineer</h1>
+
+                            <hr />
+
+                            <p>Mongo | Express | React | Node </p>
+
+                            {/* LinkedIn */}
+                            <div className="socialLinks">
+                                <a href="https://www.linkedin.com/in/mattfazza" rel="noopener noreferrer" target="_blank">
+                                    <i className="fa fa-linkedin-square" area-hidden="true" />
+                                </a>
+
+                            {/* Github */}
+                                <a href="http://www.github.com/mattfazza" rel="noopener noreferrer" target="_blank">
+                                    <i className="fa fa-github-square" area-hidden="true" />
+                                </a>
                             </div>
                         </div>
-                    </div>
+                    </Cell>
+                </Grid>
             </div>
 
         );        
