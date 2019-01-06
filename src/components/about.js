1a72df72b914695c08345d981ea1de7147a80efa
mattfazza <mathewsfazza@gmail.com>
2019-01-05 19:10:55 -0600

    Finished about me section

--- src/components/about.js
+++ src/components/about.js
@@ -8,31 +8,17 @@ class About extends Component {
             <div className = "about-outer">
                 <Grid className="landing-grid">
                     <Cell col={12}>
-                        <img 
-                            src={require("../img/linkedin.png")}
-                            alt="avatar"
-                            className="avatar-img"
-                        />
 
                         <p></p>
                         <div className="banner-text">
-                            <h1>Full Stack Engineer</h1>
-
+                            <p>Hi, I'm Matt Fazza.</p>
                             <hr />
+                            <p>I am currently a working as a software engineer at Fidelity Investments in Dallas, TX, and formerly at The Fellows Group LLC.  Because of my familiarity with Blockchain, I was working within that space before I graduated.  Upon graduating, I started working for Fidelity.  I've worked on several projects there: a full-stack web application, a data-science project, and some efforts on DevOps.  </p>
 
-                            <p>Mongo | Express | React | Node </p>
+                            <p>Although I currently work mostly with DevOps at Fidelity, my real passion lies with the MERN Stack.  I am always building something with React.js, and I really like the ecosystem.  Apart from running, taking care of my dogs, and playing music, that's how I spend most of my time.</p>
 
-                            {/* LinkedIn */}
-                            <div className="socialLinks">
-                                <a href="https://www.linkedin.com/in/mattfazza" rel="noopener noreferrer" target="_blank">
-                                    <i className="fa fa-linkedin-square" area-hidden="true" />
-                                </a>
+                            <p>About the website: I made this website thinking about mobile devices first.  Everything is friendly to both a monitor and a smartphone screen.  I think it's important to design websites with this experience in mind.  This website is built with React, Bootstrap, and Material Design Lite.  Oh!  All the pictures on this website were taken on my last trip to Chicago.</p>
 
-                            {/* Github */}
-                                <a href="http://www.github.com/mattfazza" rel="noopener noreferrer" target="_blank">
-                                    <i className="fa fa-github-square" area-hidden="true" />
-                                </a>
-                            </div>
                         </div>
                     </Cell>
                 </Grid>
