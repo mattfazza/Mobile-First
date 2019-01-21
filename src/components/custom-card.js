d66d60a683833e8dc1f7cb05850aeb7c93c1eadb
mattfazza <mathewsfazza@gmail.com>
2019-01-21 16:52:23 -0600

    started fixing some problems with the cards

--- src/components/custom-card.js
+++ src/components/custom-card.js
@@ -3,26 +3,52 @@ import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
 
 function customCard(props) {
-    return (
-        <Card className='card-parent'>
-            <CardImg top width="100%" src={props.image} alt="Card image cap" />
-            <CardBody className='card-body'>
-                <CardTitle>{props.cardTitle}</CardTitle>
-                <CardText> {props.cardText}</CardText>
 
-                <div className="linksInProjects">
-                    <a href={props.link} rel="noopener noreferrer" target="_blank">
-                        <span class="fa fa-link" area-hidden="true" ></span>
-                    </a>
+    if (props.link) {
+        return (
+            <Card className='card-parent'>
+                <CardImg top width="100%" src={props.image} alt="Card image cap" />
+                <CardBody className='card-body'>
+                    <CardTitle>{props.cardTitle}</CardTitle>
+                    <CardText> {props.cardText}</CardText>
 
-                    <a href={props.github} rel="noopener noreferrer" target="_blank">
-                        <span class="fab fa-github" area-hidden="true"></span>
-                    </a>
-                </div>
 
-            </CardBody>
-        </Card >
-    )
+                    <div className="linksInProjects">
+                        <a href={props.link} rel="noopener noreferrer" target="_blank">
+                            <span class="fa fa-link" area-hidden="true" ></span>
+                        </a>
+
+
+                        <a href={props.github} rel="noopener noreferrer" target="_blank">
+                            <span class="fab fa-github" area-hidden="true"></span>
+                        </a>
+                    </div>
+
+                </CardBody>
+            </Card >
+        )
+    } else {
+        return (
+            <Card className='card-parent'>
+                <CardImg top width="100%" src={props.image} alt="Card image cap" />
+                <CardBody className='card-body'>
+                    <CardTitle>{props.cardTitle}</CardTitle>
+                    <CardText> {props.cardText}</CardText>
+
+
+                    <div className="linksInProjects">
+                        <a href={props.github} rel="noopener noreferrer" target="_blank">
+                            <span class="fab fa-github" area-hidden="true"></span>
+                        </a>
+                    </div>
+
+                </CardBody>
+            </Card >
+        )
+
+    }
+
+
 }
 
 export default customCard;
--- src/components/projects.js
+++ src/components/projects.js
@@ -4,20 +4,38 @@ import customCard from './custom-card';
 class Projects extends Component {
 
 
+    project0 = {
+        image: "https://raw.githubusercontent.com/mattfazza/Weather/master/Initial.png",
+        cardTitle: "Mobile-first Personal Website",
+        cardText: "This website was completely made with react.  Made with React, and Reactstrap, this is a mobile-fist website.  Check it out on your phone!",
+        github: "https://github.com/mattfazza/Mobile-First"
+    }
+
     project1 = {
-        image: "https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180",
-        cardTitle: "Weather App",
-        cardText: "Look at this thing",
+        image: "https://raw.githubusercontent.com/mattfazza/Weather/master/Display.png",
+        cardTitle: "React Weather App",
+        cardText: "Search for a city and get real time weather.  Made with React, Material, and Bootstrap.  Link: UI demo only.",
         link: "https:www.google.com",
-        github: "https://www.github.com/mattfazza"
+        github: "https://github.com/mattfazza/Weather"
+    }
+
+    project2 = {
+        image: "https://raw.githubusercontent.com/mattfazza/ToDoList/master/PrintScreen.png",
+        cardTitle: "To Do App",
+        cardText: "To do list made with React",
+        link: "https://practical-carson-154b7f.netlify.com/",
+        github: "https://github.com/mattfazza/ToDoList"
     }
 
 
     render() {
         return (
             <div className='projects-outer'>
+                {customCard(this.project0)}
                 {customCard(this.project1)}
-                {customCard(this.project1)}
+                {customCard(this.project2)}
+
+                <div style={{ height: "6em" }} />
             </div>
         )
     };
